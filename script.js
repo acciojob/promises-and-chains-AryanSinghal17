3e7cb0d39bc19eafcfdcb20a206966e0d9c44d9c
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-19 21:53:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+let age = document.getElementById("age");
+let nam = document.getElementById("name");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (age.value.trim() === "" || nam.value.trim() === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const ans = new Promise((resolve, reject) => {
+
+    if (Number(age.value) >= 18) {
+      setTimeout(() => {
+        resolve(`Welcome . You can vote`);
+      }, 4000);
+    } else {
+      setTimeout(() => {
+        reject(`Oh sorry . You aren't old enough`);
+      }, 4000);
+    }
+
+  });
+
+  ans
+    .then((data) => {
+      alert(data);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+
+});
